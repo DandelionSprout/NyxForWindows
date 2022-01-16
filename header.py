b42026ec667396ebfb51e3f6b0aaba8d408fa7fd
Imre Kristoffer Eilertsen <imreeil42@gmail.com>
2022-01-16 20:14:52 +0100

    Add files via upload

---
+++ header.py
@@ -0,0 +1,512 @@
+# Copyright 2009-2019, Damian Johnson and The Tor Project
+# See LICENSE for licensing information
+
+"""
+Top panel for every page, containing basic system and tor related information.
+This expands the information it presents to two columns if there's room
+available.
+"""
+
+import os
+import time
+
+import stem
+import stem.control
+import stem.util.proc
+import stem.util.str_tools
+import stem.util.system
+
+import nyx
+import nyx.curses
+import nyx.panel
+import nyx.popups
+import nyx.tracker
+
+from stem.util import conf, log
+from nyx import nyx_interface, tor_controller
+
+from nyx.curses import RED, GREEN, YELLOW, CYAN, WHITE, BOLD, HIGHLIGHT
+
+MIN_DUAL_COL_WIDTH = 141  # minimum width where we'll show two columns
+SHOW_FD_THRESHOLD = 60  # show file descriptor usage if usage is over this percentage
+UPDATE_RATE = 5  # rate in seconds at which we refresh
+
+CONFIG = conf.config_dict('nyx', {
+  'attr.flag_colors': {},
+  'attr.version_status_colors': {},
+})
+
+
+class HeaderPanel(nyx.panel.DaemonPanel):
+  """
+  Top area containing tor settings and system information.
+  """
+
+  def __init__(self):
+    nyx.panel.DaemonPanel.__init__(self, UPDATE_RATE)
+    self._vals = Sampling.create()
+
+    self._last_width = nyx.curses.screen_size().width
+    self._reported_inactive = False
+    self._pause_time = 0
+
+    self._message = None
+    self._message_attr = []
+
+    tor_controller().add_status_listener(self._reset_listener)
+
+  def show_message(self, message = None, *attr, **kwargs):
+    """
+    Sets the message displayed at the bottom of the header. If not called with
+    anything it clears the override.
+
+    :param str message: message to be displayed
+    :param list attr: text attributes to apply
+    :param int max_wait: seconds to wait for user input, no limit if **None**
+
+    :returns: :class:`~nyx.curses.KeyInput` user pressed if provided a
+      **max_wait**, **None** otherwise or if prompt was canceled
+    """
+
+    self._message = message
+    self._message_attr = attr
+    self.redraw()
+
+    if 'max_wait' in kwargs:
+      user_input = nyx.curses.key_input(kwargs['max_wait'])
+      self.show_message()  # clear override
+      return user_input
+
+  def is_wide(self):
+    """
+    True if we should show two columns of information, False otherwise.
+    """
+
+    return self._last_width >= MIN_DUAL_COL_WIDTH
+
+  def get_height(self):
+    """
+    Provides the height of the content, which is dynamically determined by the
+    panel's maximum width.
+    """
+
+    max_height = nyx.panel.DaemonPanel.get_height(self)
+
+    if self._vals.is_relay:
+      return min(max_height, 5 if self.is_wide() else 7)
+    else:
+      return min(max_height, 4 if self.is_wide() else 5)
+
+  def send_newnym(self):
+    """
+    Requests a new identity and provides a visual queue.
+    """
+
+    controller = tor_controller()
+
+    if not controller.is_newnym_available():
+      return
+
+    controller.signal(stem.Signal.NEWNYM)
+
+    # If we're wide then the newnym label in this panel will give an
+    # indication that the signal was sent. Otherwise use a msg.
+
+    if not self.is_wide():
+      self.show_message('Requesting a new identity', HIGHLIGHT, max_wait = 1)
+
+  def set_paused(self, is_pause):
+    if is_pause:
+      self._pause_time = time.time()
+
+  def key_handlers(self):
+    def _reconnect():
+      if self._vals.is_connected:
+        return
+
+      controller = tor_controller()
+      self.show_message('Reconnecting...', HIGHLIGHT)
+
+      try:
+        try:
+          controller.reconnect(chroot_path = nyx.chroot())
+        except stem.connection.MissingPassword:
+          password = nyx.input_prompt('Controller Password: ')
+
+          if password:
+            controller.authenticate(password)
+
+        log.notice("Reconnected to Tor's control port")
+        self.show_message('Tor reconnected', HIGHLIGHT, max_wait = 1)
+      except Exception as exc:
+        self.show_message('Unable to reconnect (%s)' % exc, HIGHLIGHT, max_wait = 3)
+        controller.close()
+
+    return (
+      nyx.panel.KeyHandler('n', action = self.send_newnym),
+      nyx.panel.KeyHandler('r', action = _reconnect),
+    )
+
+  def _draw(self, subwindow):
+    vals = self._vals  # local reference to avoid concurrency concerns
+    self._last_width = subwindow.width
+    is_wide = self.is_wide()
+
+    # space available for content
+
+    interface = nyx_interface()
+    left_width = max(subwindow.width // 2, 77) if is_wide else subwindow.width
+    right_width = subwindow.width - left_width
+
+    _draw_platform_section(subwindow, 0, 0, left_width, vals)
+
+    if vals.is_connected:
+      _draw_ports_section(subwindow, 0, 1, left_width, vals)
+    else:
+      _draw_disconnected(subwindow, 0, 1, vals.last_heartbeat)
+
+    if is_wide:
+      _draw_resource_usage(subwindow, left_width, 0, right_width, vals, self._pause_time)
+
+      if vals.is_relay:
+        _draw_fingerprint_and_fd_usage(subwindow, left_width, 1, right_width, vals)
+        _draw_flags(subwindow, 0, 2, vals.flags)
+        _draw_exit_policy(subwindow, left_width, 2, vals.exit_policy)
+      elif vals.is_connected:
+        _draw_newnym_option(subwindow, left_width, 1, vals.newnym_wait)
+    else:
+      _draw_resource_usage(subwindow, 0, 2, left_width, vals, self._pause_time)
+
+      if vals.is_relay:
+        _draw_fingerprint_and_fd_usage(subwindow, 0, 3, left_width, vals)
+        _draw_flags(subwindow, 0, 4, vals.flags)
+
+    _draw_status(subwindow, 0, self.get_height() - 1, interface.is_paused(), self._message, *self._message_attr)
+
+  def _reset_listener(self, controller, event_type, _):
+    self._update()
+
+    if event_type == stem.control.State.CLOSED:
+      log.notice('Tor control port closed')
+
+  def _update(self):
+    self._vals = Sampling.create(self._vals)
+
+    if self._vals.fd_used and self._vals.fd_limit != -1:
+      fd_percent = 100 * self._vals.fd_used // self._vals.fd_limit
+
+      if fd_percent >= 90:
+        log_msg = "Tor's file descriptor usage is at %s%%. If you run out Tor will be unable to continue functioning." % fd_percent
+        log.log_once('fd_used_at_ninety_percent', log.WARN, log_msg)
+        log.DEDUPLICATION_MESSAGE_IDS.add('fd_used_at_sixty_percent')
+      elif fd_percent >= 60:
+        log_msg = "Tor's file descriptor usage is at %s%%." % fd_percent
+        log.log_once('fd_used_at_sixty_percent', log.NOTICE, log_msg)
+
+    if self._vals.is_connected:
+      if not self._reported_inactive and (time.time() - self._vals.last_heartbeat) >= 10:
+        self._reported_inactive = True
+        log.notice('Relay unresponsive (last heartbeat: %s)' % time.ctime(self._vals.last_heartbeat))
+      elif self._reported_inactive and (time.time() - self._vals.last_heartbeat) < 10:
+        self._reported_inactive = False
+        log.notice('Relay resumed')
+
+    self.redraw()
+
+
+class Sampling(object):
+  def __init__(self, **attr):
+    self._attr = attr
+
+    for key, value in attr.items():
+      setattr(self, key, value)
+
+  @staticmethod
+  def create(last_sampling = None):
+    controller = tor_controller()
+    retrieved = time.time()
+
+    pid = controller.get_pid('')
+    tor_resources = nyx.tracker.get_resource_tracker().get_value()
+    nyx_total_cpu_time = sum(os.times()[:3], stem.util.system.SYSTEM_CALL_TIME)
+
+    or_listeners = controller.get_listeners(stem.control.Listener.OR, [])
+    control_listeners = controller.get_listeners(stem.control.Listener.CONTROL, [])
+    my_router_status_entry = nyx.tracker.get_consensus_tracker().my_router_status_entry()
+
+    if controller.get_conf('HashedControlPassword', None):
+      auth_type = 'password'
+    elif controller.get_conf('CookieAuthentication', None) == '1':
+      auth_type = 'cookie'
+    else:
+      auth_type = 'open'
+
+    try:
+      fd_used = stem.util.proc.file_descriptors_used(pid)
+    except IOError:
+      fd_used = None
+
+    if last_sampling:
+      nyx_cpu_delta = nyx_total_cpu_time - last_sampling.nyx_total_cpu_time
+      nyx_time_delta = retrieved - last_sampling.retrieved
+      nyx_cpu = nyx_cpu_delta / nyx_time_delta
+    else:
+      nyx_cpu = 0.0
+
+    attr = {
+      'retrieved': retrieved,
+      'is_connected': controller.is_alive(),
+      'connection_time': controller.connection_time(),
+      'last_heartbeat': controller.get_latest_heartbeat(),
+
+      'fingerprint': controller.get_info('fingerprint', 'Unknown'),
+      'nickname': controller.get_conf('Nickname', ''),
+      'newnym_wait': controller.get_newnym_wait(),
+      'exit_policy': controller.get_exit_policy(None),
+      'flags': getattr(my_router_status_entry, 'flags', []),
+
+      'version': str(controller.get_version('Unknown')).split()[0],
+      'version_status': controller.get_info('status/version/current', 'Unknown'),
+
+      'address': or_listeners[0][0] if (or_listeners and or_listeners[0][0] != '0.0.0.0') else controller.get_info('address', 'Unknown'),
+      'or_port': or_listeners[0][1] if or_listeners else '',
+      'dir_port': controller.get_conf('DirPort', '0'),
+      'control_port': str(control_listeners[0][1]) if control_listeners else None,
+      'socket_path': controller.get_conf('ControlSocket', None),
+      'is_relay': bool(or_listeners),
+
+      'auth_type': auth_type,
+      'pid': pid,
+      'start_time': stem.util.system.start_time(pid),
+      'fd_limit': int(controller.get_info('process/descriptor-limit', '-1')),
+      'fd_used': fd_used,
+
+      'nyx_total_cpu_time': nyx_total_cpu_time,
+      'tor_cpu': '%0.1f' % (100 * tor_resources.cpu_sample),
+      'nyx_cpu': '%0.1f' % (100 * nyx_cpu),
+      'memory': stem.util.str_tools.size_label(tor_resources.memory_bytes) if tor_resources.memory_bytes > 0 else 0,
+      'memory_percent': '%0.1f' % (100 * tor_resources.memory_percent),
+
+      'hostname': os,
+      'platform': 'Windows 11'
+    }
+
+    return Sampling(**attr)
+
+  def format(self, message, crop_width = None):
+    formatted_msg = message.format(**self._attr)
+
+    if crop_width is not None:
+      formatted_msg = stem.util.str_tools.crop(formatted_msg, crop_width)
+
+    return formatted_msg
+
+
+def _draw_platform_section(subwindow, x, y, width, vals):
+  """
+  Section providing the user's hostname, platform, and version information...
+
+    nyx - odin (Linux 3.5.0-52-generic)        Tor 0.2.5.1-alpha-dev (unrecommended)
+    |------ platform (40 characters) ------|   |----------- tor version -----------|
+  """
+
+  initial_x, space_left = x, min(width, 40)
+
+  x = subwindow.addstr(x, y, vals.format('nyx - {hostname}', space_left))
+  space_left -= x - initial_x
+
+  if space_left >= 10:
+    subwindow.addstr(x, y, ' (%s)' % vals.format('{platform}', space_left - 3))
+
+  x, space_left = initial_x + 43, width - 43
+
+  if vals.version != 'Unknown' and space_left >= 10:
+    x = subwindow.addstr(x, y, vals.format('Tor {version}', space_left))
+    space_left -= x - 43 - initial_x
+
+    if space_left >= 7 + len(vals.version_status):
+      version_color = CONFIG['attr.version_status_colors'].get(vals.version_status, WHITE)
+
+      x = subwindow.addstr(x, y, ' (')
+      x = subwindow.addstr(x, y, vals.version_status, version_color)
+      subwindow.addstr(x, y, ')')
+
+
+def _draw_ports_section(subwindow, x, y, width, vals):
+  """
+  Section providing our nickname, address, and port information...
+
+    Unnamed - 0.0.0.0:7000, Control Port (cookie): 9051
+  """
+
+  if not vals.is_relay:
+    x = subwindow.addstr(x, y, 'Relaying Disabled', CYAN)
+  else:
+    x = subwindow.addstr(x, y, vals.format('{nickname} - {address}:{or_port}'))
+
+    if vals.dir_port != '0':
+      x = subwindow.addstr(x, y, vals.format(', Dir Port: {dir_port}'))
+
+  if vals.control_port:
+    if width >= x + 19 + len(vals.control_port) + len(vals.auth_type):
+      auth_color = RED if vals.auth_type == 'open' else GREEN
+
+      x = subwindow.addstr(x, y, ', Control Port (')
+      x = subwindow.addstr(x, y, vals.auth_type, auth_color)
+      subwindow.addstr(x, y, vals.format('): {control_port}'))
+    else:
+      subwindow.addstr(x, y, vals.format(', Control Port: {control_port}'))
+  elif vals.socket_path:
+    subwindow.addstr(x, y, vals.format(', Control Socket: {socket_path}'))
+
+
+def _draw_disconnected(subwindow, x, y, last_heartbeat):
+  """
+  Message indicating that tor is disconnected...
+
+    Tor Disconnected (15:21 07/13/2014, press r to reconnect)
+  """
+
+  x = subwindow.addstr(x, y, 'Tor Disconnected', RED, BOLD)
+  last_heartbeat_str = time.strftime('%H:%M %m/%d/%Y', time.localtime(last_heartbeat))
+  subwindow.addstr(x, y, ' (%s, press r to reconnect)' % last_heartbeat_str)
+
+
+def _draw_resource_usage(subwindow, x, y, width, vals, pause_time):
+  """
+  System resource usage of the tor process...
+
+    cpu: 0.0% tor, 1.0% nyx    mem: 0 (0.0%)       pid: 16329  uptime: 12-20:42:07
+  """
+
+  if vals.start_time:
+    if not vals.is_connected:
+      now = vals.connection_time
+    elif pause_time:
+      now = pause_time
+    else:
+      now = time.time()
+
+    uptime = stem.util.str_tools.short_time_label(max(0, now - vals.start_time))
+  else:
+    uptime = ''
+
+  sys_fields = (
+    (0, vals.format('cpu: {tor_cpu}% tor, {nyx_cpu}% nyx')),
+    (27, vals.format('mem: {memory} ({memory_percent}%)')),
+    (47, vals.format('pid: {pid}')),
+    (59, 'uptime: %s' % uptime),
+  )
+
+  for (start, label) in sys_fields:
+    if width >= start + len(label):
+      subwindow.addstr(x + start, y, label)
+    else:
+      break
+
+
+def _draw_fingerprint_and_fd_usage(subwindow, x, y, width, vals):
+  """
+  Presents our fingerprint, and our file descriptor usage if we're running
+  out...
+
+    fingerprint: 1A94D1A794FCB2F8B6CBC179EF8FDD4008A98D3B, file desc: 900 / 1000 (90%)
+  """
+
+  initial_x, space_left = x, width
+
+  x = subwindow.addstr(x, y, vals.format('fingerprint: {fingerprint}', width))
+  space_left -= x - initial_x
+
+  if space_left >= 30 and vals.fd_used and vals.fd_limit != -1:
+    fd_percent = 100 * vals.fd_used / vals.fd_limit
+
+    if fd_percent >= SHOW_FD_THRESHOLD:
+      if fd_percent >= 95:
+        percentage_format = (RED, BOLD)
+      elif fd_percent >= 90:
+        percentage_format = (RED,)
+      elif fd_percent >= 60:
+        percentage_format = (YELLOW,)
+      else:
+        percentage_format = ()
+
+      x = subwindow.addstr(x, y, ', file descriptors' if space_left >= 37 else ', file desc')
+      x = subwindow.addstr(x, y, vals.format(': {fd_used} / {fd_limit} ('))
+      x = subwindow.addstr(x, y, '%i%%' % fd_percent, *percentage_format)
+      subwindow.addstr(x, y, ')')
+
+
+def _draw_flags(subwindow, x, y, flags):
+  """
+  Presents flags held by our relay...
+
+    flags: Running, Valid
+  """
+
+  x = subwindow.addstr(x, y, 'flags: ')
+
+  if flags:
+    for i, flag in enumerate(flags):
+      flag_color = CONFIG['attr.flag_colors'].get(flag, WHITE)
+      x = subwindow.addstr(x, y, flag, flag_color, BOLD)
+
+      if i < len(flags) - 1:
+        x = subwindow.addstr(x, y, ', ')
+  else:
+    subwindow.addstr(x, y, 'none', CYAN, BOLD)
+
+
+def _draw_exit_policy(subwindow, x, y, exit_policy):
+  """
+  Presents our exit policy...
+
+    exit policy: reject *:*
+  """
+
+  x = subwindow.addstr(x, y, 'exit policy: ')
+
+  if not exit_policy:
+    return
+
+  rules = list(exit_policy.strip_private().strip_default())
+
+  for i, rule in enumerate(rules):
+    policy_color = GREEN if rule.is_accept else RED
+    x = subwindow.addstr(x, y, str(rule), policy_color, BOLD)
+
+    if i < len(rules) - 1:
+      x = subwindow.addstr(x, y, ', ')
+
+  if exit_policy.has_default():
+    if rules:
+      x = subwindow.addstr(x, y, ', ')
+
+    subwindow.addstr(x, y, '<default>', CYAN, BOLD)
+
+
+def _draw_newnym_option(subwindow, x, y, newnym_wait):
+  """
+  Provide a notice for requiesting a new identity, and time until it's next
+  available if in the process of building circuits.
+  """
+
+  if newnym_wait == 0:
+    subwindow.addstr(x, y, "press 'n' for a new identity")
+  else:
+    plural = 's' if newnym_wait > 1 else ''
+    subwindow.addstr(x, y, 'building circuits, available again in %i second%s' % (newnym_wait, plural))
+
+
+def _draw_status(subwindow, x, y, is_paused, message, *attr):
+  """
+  Provides general usage information or a custom message.
+  """
+
+  if message:
+    subwindow.addstr(x, y, message, *attr)
+  elif not is_paused:
+    interface = nyx_interface()
+    subwindow.addstr(x, y, 'page %i / %i - m: menu, p: pause, h: page help, q: quit' % (interface.get_page() + 1, interface.page_count()))
+  else:
+    subwindow.addstr(x, y, 'Paused', HIGHLIGHT)
---
+++ starter.py
@@ -0,0 +1,256 @@
+# Copyright 2009-2019, Damian Johnson and The Tor Project
+# See LICENSE for licensing information
+
+"""
+Command line application for monitoring Tor relays, providing real time status
+information. This starts the application, parsing arguments and getting a Tor
+connection.
+"""
+
+import locale
+import logging
+import os
+import platform
+import sys
+import time
+
+import nyx
+import nyx.arguments
+import nyx.curses
+import nyx.tracker
+
+import stem
+import stem.connection
+import stem.util.log
+import stem.util.system
+
+from nyx import init_controller, uses_settings, nyx_interface
+
+DEBUG_HEADER = """
+Nyx {nyx_version} Debug Dump
+Stem Version: {stem_version}
+Python Version: {python_version}
+Platform: {system} ({platform})
+--------------------------------------------------------------------------------
+Nyx Configuration ({nyxrc_path}):
+{nyxrc_content}
+--------------------------------------------------------------------------------
+""".strip()
+
+TORRC = """
+--------------------------------------------------------------------------------
+Torrc ({torrc_path}):
+{torrc_content}
+--------------------------------------------------------------------------------
+""".rstrip()
+
+
+@uses_settings
+def main(config):
+  config.set('start_time', str(int(time.time())))
+
+  try:
+    args = nyx.arguments.parse(sys.argv[1:])
+    config.set('logged_events', args.logged_events)
+  except ValueError as exc:
+    print(exc)
+    sys.exit(1)
+
+  if args.print_help:
+    print(nyx.arguments.get_help())
+    sys.exit()
+  elif args.print_version:
+    print(nyx.arguments.get_version())
+    sys.exit()
+
+  if args.debug_path is not None:
+    try:
+      _setup_debug_logging(args)
+      print('Saving a debug log to %s, please check it for sensitive information before sharing it.' % args.debug_path)
+    except IOError as exc:
+      print('Unable to write to our debug log file (%s): %s' % (args.debug_path, exc.strerror))
+      sys.exit(1)
+
+  if os.path.exists(args.config):
+    try:
+      config.load(args.config)
+    except IOError as exc:
+      stem.util.log.warn('Failed to load configuration (using defaults): "%s"' % exc.strerror)
+  else:
+    stem.util.log.notice('No nyxrc loaded, using defaults. You can customize nyx by placing a configuration file at %s (see https://nyx.torproject.org/nyxrc.sample for its options).' % args.config)
+
+  # If a password is provided via the user's nyxrc that will be use, otherwise
+  # users are prompted for a password if required.
+
+  controller_password = config.get('password', None)
+
+  if controller_password:
+    stem.connection.CONNECT_MESSAGES['incorrect_password'] = 'Unable to authenticate to tor using the controller password in %s' % args.config
+
+  controller = init_controller(
+    control_port = args.control_port,
+    control_socket = args.control_socket,
+    password = controller_password,
+    password_prompt = True,
+    chroot_path = nyx.chroot(),
+  )
+
+  if controller is None:
+    exit(1)
+
+  if args.debug_path is not None:
+    torrc_path = controller.get_info('config-file')
+
+    try:
+      with open(torrc_path) as torrc_file:
+        torrc_content = torrc_file.read()
+    except Exception as exc:
+      torrc_content = 'Unable to read %s: %s' % (torrc_path, exc)
+
+    stem.util.log.trace(TORRC.format(torrc_path = torrc_path, torrc_content = torrc_content))
+
+  _warn_if_root(controller)
+  _warn_if_unable_to_get_pid(controller)
+  _warn_about_unused_config_keys()
+  _use_unicode()
+  _set_process_name()
+
+  # These os.putenv calls fail on FreeBSD, and even attempting causes python to
+  # print the following to stdout...
+  #
+  #   nyx: environment corrupt; missing value for
+
+  if not stem.util.system.is_bsd():
+    os.putenv('LANG', 'C')  # make subcommands (ps, netstat, etc) provide english results
+    os.putenv('ESCDELAY', '0')  # make 'esc' take effect right away
+
+  try:
+    nyx.curses.start(nyx.draw_loop, acs_support = config.get('acs_support', True), transparent_background = True, cursor = False)
+  except KeyboardInterrupt:
+    pass  # skip printing a stack trace
+  finally:
+    nyx.curses.halt()
+    _shutdown_daemons(controller)
+
+
+def _setup_debug_logging(args):
+  """
+  Configures us to log at stem's trace level to a debug log path. This starts
+  it off with some general diagnostic information.
+  """
+
+  debug_dir = os.path.dirname(args.debug_path)
+
+  if debug_dir and not os.path.exists(debug_dir):
+    os.makedirs(debug_dir)
+
+  debug_handler = logging.FileHandler(args.debug_path, mode = 'w')
+  debug_handler.setLevel(stem.util.log.logging_level(stem.util.log.TRACE))
+  debug_handler.setFormatter(logging.Formatter(
+    fmt = '%(asctime)s [%(levelname)s] %(message)s',
+    datefmt = '%m/%d/%Y %H:%M:%S'
+  ))
+
+  logger = stem.util.log.get_logger()
+  logger.addHandler(debug_handler)
+
+  nyxrc_content = "[file doesn't exist]"
+
+  if os.path.exists(args.config):
+    try:
+      with open(args.config) as nyxrc_file:
+        nyxrc_content = nyxrc_file.read()
+    except IOError as exc:
+      nyxrc_content = '[unable to read file: %s]' % exc.strerror
+
+  stem.util.log.trace(DEBUG_HEADER.format(
+    nyx_version = nyx.__version__,
+    stem_version = stem.__version__,
+    python_version = '.'.join(map(str, sys.version_info[:3])),
+    system = platform.system(),
+    platform = ' '.join(platform.dist()),
+    nyxrc_path = args.config,
+    nyxrc_content = nyxrc_content,
+  ))
+
+
+def _warn_if_root(controller):
+  """
+  Give a notice if tor or nyx are running with root.
+  """
+
+  if controller.get_user(None) == 'root':
+    stem.util.log.notice("Tor is currently running with root permissions. This isn't a good idea, nor should it be necessary. See the 'User UID' option on Tor's man page for an easy method of reducing its permissions after startup.")
+
+
+def _warn_if_unable_to_get_pid(controller):
+  """
+  Provide a warning if we're unable to determine tor's pid. This in turn will
+  limit our ability to query information about the process later.
+  """
+
+  try:
+    controller.get_pid()
+  except ValueError:
+    stem.util.log.warn("Unable to determine Tor's pid. Some information, like its resource usage will be unavailable.")
+
+
+@uses_settings
+def _warn_about_unused_config_keys(config):
+  """
+  Provides a notice if the user's nyxrc has any entries that are unused.
+  """
+
+  for key in sorted(config.unused_keys()):
+    if not key.startswith('msg.') and not key.startswith('dedup.'):
+      stem.util.log.notice('Unused configuration entry: %s' % key)
+
+
+@uses_settings
+def _use_unicode(config):
+  """
+  If using our LANG variable for rendering multi-byte characters lets us
+  get unicode support then then use it. This needs to be done before
+  initializing curses.
+  """
+
+  if config.get('unicode_support', True):
+    is_lang_unicode = 'utf-' in os.getenv('LANG', '').lower()
+
+    if is_lang_unicode and nyx.curses.is_wide_characters_supported():
+      locale.setlocale(locale.LC_ALL, '')
+
+
+def _set_process_name():
+  """
+  Attempts to rename our process from "python setup.py <input args>" to
+  "nyx <input args>".
+  """
+
+  process_name = 'nyx\0%s' % '\0'.join(sys.argv[1:])
+
+  try:
+    stem.util.system.set_process_name(process_name)
+  except IOError as exc:
+    stem.util.log.info("Unable to rename our process from '%s' to '%s' (%s)." % (stem.util.system.get_process_name(), process_name.replace('\0', ' '), exc))
+
+
+def _shutdown_daemons(controller):
+  """
+  Stops and joins on worker threads.
+  """
+
+  halt_threads = [nyx.tracker.stop_trackers()]
+  interface = nyx_interface()
+
+  if interface:
+    halt_threads.append(interface.halt())
+
+  for thread in halt_threads:
+    thread.join()
+
+  controller.close()
+
+
+if __name__ == '__main__':
+  main()
