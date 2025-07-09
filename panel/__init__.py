972d293f0338a0d3f45ef1aa1dfe2081794e9b1d
Imre Eilertsen <imreeil42@gmail.com>
2025-07-09 07:53:07 +0200

    Next time I should read the comments of something
    And not just the OP. I don't blame that OP's poster, worth noting.

--- panel/__init__.py
+++ panel/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2010-2019, Damian Johnson and The Tor Project
+# Copyright 2010-2025, Damian Johnson, The Tor Project, Imre Eilertsen
 # See LICENSE for licensing information
 
 """
@@ -78,7 +78,7 @@ class KeyHandler(collections.namedtuple('Help', ['key', 'description', 'current'
       is_match = self._key_func(key) if self._key_func else key.match(self.key)
 
       if is_match:
-        if inspect.signature(self._action).args == ['key']:
+        if inspect.getfullargspec(self._action).args == ['key']:
           self._action(key)
         else:
           self._action()
